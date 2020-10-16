90302d129ae232b9dffc1c1a0187d3501420db35
Artur-Petriv <artur.petriv@gmail.com>
2020-10-16 12:38:02 +0300

    Task 22

---
+++ Task 22/index.js
@@ -0,0 +1,20 @@
+/*
+Написать код, который из массива сделает обьект.
+*/
+
+// Моё решение 1
+function transform(arr) {
+  let obj = {
+    name: "",
+    age: "",
+  };
+
+  arr.forEach((elem) => {});
+}
+
+const arr = [
+  { name: "name", value: "Анатолий" },
+  { name: "age", value: 40 },
+];
+
+console.log(transform(arr)); // {name: 'Анатолій', age: 40}
